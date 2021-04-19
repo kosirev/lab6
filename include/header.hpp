67dea9a7de55c8f5998655ebe528fd015b0082ba
kosirev <uchheba@gmail.com>
2021-04-20 02:31:02 +0300

    First

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,97 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <iostream>
+#include "../third-party/PicoSHA2/picosha2.h"
+#include <string>
+#include <chrono>
+#include <thread>
+#include <csignal>
+#include <nlohmann/json.hpp>
+#include <mutex>
+#include <vector>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/smart_ptr/make_shared_object.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/attributes/timer.hpp>
+#include <boost/log/attributes/named_scope.hpp>
+
+using time_point = std::chrono::system_clock::time_point;
+using high_clock = std::chrono::high_resolution_clock;
+using std::cout;
+using std::endl;
+using string = std::string;
+using json = nlohmann::json;
+using boost::shared_ptr;
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+using namespace logging::trivial;
+namespace attrs = boost::log::attributes;
+namespace expr = boost::log::expressions;
+
+static const string Hex_End = "0000";
+
+class Hasher {
+ public:
+  Hasher(int argc, char *argv[]);
+
+  ~Hasher();
+
+  void static signal_catch(int signum){
+    write_to_json_file("hash_hex_log.json");
+    sleep(1);
+    std::cout << "\nProgram aborted with code " << --signum << "\n";
+    exit(signum);
+  }
+
+  void static write_to_json_file(const string& file_name){
+    std::ofstream output_file;
+    std::ifstream input_file;
+    output_file.open(file_name);
+    input_file.open(file_name);
+    json out_json;
+    if(input_file.peek() != EOF)
+      input_file >> out_json;
+    for(auto & right_hash : right_hashs) {
+      out_json["values"].push_back(right_hash);
+    }
+    output_file << out_json.dump(4);
+    output_file.close();
+  };
+
+  void start(const bool& key);
+
+  void sort_hash(string& hash);
+
+  void do_hashing(const bool& key);
+ private:
+  std::mutex mutex;
+  unsigned int number_threads;
+  long duration;
+  string src_str;
+  string hash_hex_str;
+  std::vector<std::thread> threads;
+  static std::vector<json> right_hashs;
+  src::severity_logger< severity_level > slg;
+};
 
 #endif // INCLUDE_HEADER_HPP_
